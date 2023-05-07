f739eabfbb9abcfd46fe4c7cfeb3ad5e7562126f
Giorgi Adeishvili <37007370+georgemff@users.noreply.github.com>
2023-05-07 13:39:05 +0400

    Create Beautiful Matrix - 263A.cpp

---
+++ Beautiful Matrix - 263A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int matrix[5][5], i, a, b, c, j;
+main(){
+   for(i=0; i<5; i++){
+      for(j=0; j<5; j++)
+         cin>>matrix[i][j];
+   }
+   for(i=0; i<5; i++){
+     for(j=0; j<5; j++){
+        if(matrix[i][j]==1){
+            a=i;
+            b=j;
+        }
+     }
+   }
+   if(a==0){
+     if(b<2){c=(2-b)+2;}
+     if(b>2){c=(b-2)+2;}
+     if(b==2){c=c+2;}
+   }
+   if(a==1){
+     if(b<2){c=(2-b)+1;}
+     if(b>2){c=(b-2)+1;}
+     if(b==2){c=c+1;}
+   }
+   if(a==2){
+     if(b<2){c=(2-b)+0;}
+     if(b>2){c=(b-2)+0;}
+     if(b==2){c=c+0;}
+   }
+   if(a==3){
+     if(b<2){c=(2-b)+1;}
+     if(b>2){c=(b-2)+1;}
+     if(b==2){c=c+1;}
+   }
+   if(a==4){
+     if(b<2){c=(2-b)+2;}
+     if(b>2){c=(b-2)+2;}
+     if(b==2){c=c+2;}
+   }
+   cout<<c;
+}
+ 
